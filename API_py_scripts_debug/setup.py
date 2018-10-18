06197a0f46b5ba86052b91dbfda5240af0fa0e6c
lirhlove2016 <781717724@qq.com>
2018-10-18 15:29:49 +0800

    setup.py commit
    setup.py commit

--- API_py_scripts_debug/setup.py
+++ API_py_scripts_debug/setup.py
@@ -1,57 +1,17 @@
 # _*_ coding:utf-8 _*_
 import os
 
-"""
-实现自动安装库文件1.xlrd，2.xlwt
+try:
+    import requests
+except :
+    os.system('pip install requests')
 
-"""
-path="E:\download\setup"
+try:
+    import xlrd
+except :
+    os.system('pip install xlrd')
 
-
-def setup_openpyxl():
-    try:
-        from openpyxl import load_workbook
-    except:
-        print('\nnow install "openxl"')
-        #set "setup.py" file path with yours
-        setup = os.path.join(path, 'dependence', 'openpyxl-2.5.8', 'setup.py')        
-        cwd = os.getcwd()
-        os.chdir(os.path.dirname(setup))
-        os.system(' '.join(('python', setup, 'install')))
-        os.chdir(cwd)
-        from openpyxl import load_workbook
-        
-    return load_workbook
-
-def setup_xlrd():
-    try:
-        from xlrd import open_workbook
-    except:
-        print('\nnow install "xlrd"')
-        setup = os.path.join(path, 'dependence', 'xlrd-1.1.0', 'setup.py')        
-        cwd = os.getcwd()
-        os.chdir(os.path.dirname(setup))
-        os.system(' '.join(('python', setup, 'install')))
-        os.chdir(cwd)
-        from xlrd import load_workbook
-        
-    return open_workbook
-
-def setup_xlwt():
-    try:
-        from xlwt import Workbook        
-    except:
-        print('\nnow install "xlwt"')
-        setup = os.path.join(path, 'dependence', 'xlwt-1.3.0', 'setup.py')        
-        cwd = os.getcwd()
-        os.chdir(os.path.dirname(setup))
-        os.system(' '.join(('python', setup, 'install')))
-        os.chdir(cwd)
-        from xlwt import Workbook
-        
-    return Workbook
-
-if __name__ == '__main__':
-    xlrd = setup_xlrd()
-    xlwt = setup_xlwt()
-    
+try:
+    import xlwt
+except :
+    os.system('pip install xlwt')
